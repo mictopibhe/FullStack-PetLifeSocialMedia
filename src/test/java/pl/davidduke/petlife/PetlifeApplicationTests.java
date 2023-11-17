aa3bf3b247b3d8b2d38fc3837d679beb154c0e68
mictopibhe <mictopibhe@gmail.com>
2023-11-17 20:43:49 +0100

    first push

---
+++ src/test/java/pl/davidduke/petlife/PetlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.davidduke.petlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
